c94cbc5112a4dc60b3b5f2e7195dc6312c498617
thchfhch1 <140287954+thchfhch1@users.noreply.github.com>
2023-08-11 17:15:42 +0800

    well, why I can't commit

--- files/random-foods/main.js
+++ files/random-foods/main.js
@@ -1,22 +1,44 @@
 const randButton = document.querySelector('button');
+const checkBox = document.querySelector('input');
 const para = document.querySelector('h2');
 const cook = ['清蒸', '红烧', '爆炒', '大火慢炖', '串烧', '碳烤', '油炸', '凉拌', '油焖',
               '水煮', '风干', '盐焗'];
-const food = ['遗迹', '精灵球', '尝氨使者', '[th]', '答辩', '蛐蛐汽车', '玄月'];
+const food = ['遗迹', '精灵球', '尝氨使者', '答辩', '蛐蛐汽车', '玄月', 'A.Square'];
+let randomColorOn = 1;
 
-function ramdom(num)
+function randomToNum(num)
 {
     return Math.floor(Math.random() * num);
+}
+function random(l, r)
+{
+    return l + randomToNum(r - l);
 }
 
 function randomElement(array)
 {
-    return array[ramdom(array.length)];
+    return array[randomToNum(array.length)];
 }
+function randomColor()
+{
+    return ('rgb(' +
+            random(0, 255) +
+            ',' +
+            random(0, 255) + 
+            ',' +
+            random(0, 255) +
+            ')');
+}
+
+checkBox.addEventListener('change', () => 
+{
+    randomColorOn = !randomColorOn;
+});
 
 randButton.addEventListener('click', () => 
 {
     let s = randomElement(cook) + randomElement(food);
     para.textContent = s;
+    para.style.color = (randomColorOn ? randomColor() : 'orange');
 }
 );
